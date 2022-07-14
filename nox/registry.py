dc2b76f502c5eda3ded37402af747e86248a2331
Ganden Schaffner <gschaffner@pm.me>
2022-07-14 00:00:00 -0700

    Support using another sessions's venv

--- nox/registry.py
+++ nox/registry.py
@@ -43,6 +43,7 @@ def session_decorator(
     venv_params: Any = ...,
     tags: list[str] | None = ...,
     requires: list[str] | None = ...,
+    venv: str | None = ...,
 ) -> Callable[[F], F]:
     ...
 
@@ -57,6 +58,7 @@ def session_decorator(
     venv_params: Any = None,
     tags: list[str] | None = None,
     requires: list[str] | None = None,
+    venv: str | None = None,
 ) -> F | Callable[[F], F]:
     """Designate the decorated function as a session."""
     # If `func` is provided, then this is the decorator call with the function
@@ -77,6 +79,7 @@ def session_decorator(
             venv_params=venv_params,
             tags=tags,
             requires=requires,
+            venv=venv,
         )
 
     if py is not None and python is not None:
@@ -88,6 +91,15 @@ def session_decorator(
     if python is None:
         python = py
 
+    if venv is not None:
+        for param_name in ("reuse_venv", "venv_backend", "venv_params"):
+            var = locals()[param_name]
+            if var is not None:
+                raise ValueError(
+                    f"The {param_name} argument to nox.session cannot be used when"
+                    " using the venv argument."
+                )
+
     fn = Func(
         func,
         python,
@@ -97,6 +109,7 @@ def session_decorator(
         venv_params,
         tags=tags,
         requires=requires,
+        venv=venv,
     )
     _REGISTRY[name or func.__name__] = fn
     return fn
